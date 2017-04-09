d7a5443c881900e88ed02a77194a9b3e663909eb
Jamian Vieira <Jamianvieira@gmail.com>
2017-04-08 20:25:05 -0700

    Added Initial Project
    Includes shooting script and some basic colliders on the player and
    walls.

---
+++ 380_Game/Assets/BulletScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		    
+	}
+
+	void OnBecameInvisible(){
+		Destroy(this.gameObject);
+	}
+}
---
+++ 380_Game/Assets/Shooting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	//bulletprefab for bullet object
+	//speed variable controls bullet speed
+	//bullet distance is how far away the bullet spawns from the player
+	public GameObject bulletPrefab;
+	public float speed = 5.0f;
+	public float bulletDistance = 0.5f;
+	private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			//where the mouse is pointing
+			Vector3 worldMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+			//direction the bullet will go in
+			direction = (Vector2)(worldMousePos - transform.position);
+			direction.Normalize ();
+
+			//fire bullet
+			fire ();
+		}
+	}
+
+	void fire(){
+		//Instantiate bullet locally
+		GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position + (Vector3)(direction * bulletDistance), Quaternion.identity);
+
+		//add velocity to bullet
+		bullet.GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+
+}
