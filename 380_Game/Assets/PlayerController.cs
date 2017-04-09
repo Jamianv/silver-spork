c8973d48ca45ed56f008a8632fccaa8b52f82f3a
Jamian Vieira <Jamianvieira@gmail.com>
2017-04-09 02:51:07 -0700

    Added script which uses its own physics to manipulate objects rather than unity's system. Added character controller that functions off of this system.

---
+++ 380_Game/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject {
+
+	public float maxspeed = 7;
+	public float jumpTakeOffSpeed = 7;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	protected override void ComputeVelocity(){
+		Vector2 move = Vector2.zero;
+
+		move.x = Input.GetAxis ("Horizontal");
+
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			velocity.y = jumpTakeOffSpeed;
+		}
+		else if(Input.GetButtonUp("Jump")){
+			if (velocity.y > 0)
+				velocity.y *= .5f;
+		}
+
+		targetVelocity = move * maxspeed;
+	}
+}
