4a349587910164383da2d803bdd1261abacbb64d
Jamian Vieira <Jamianvieira@gmail.com>
2017-04-23 03:05:20 -0700

    Added deadzone camera

--- 380_Game/Assets/Scripts/FollowCam.cs
+++ 380_Game/Assets/Scripts/FollowCam.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//!!!ALL PUBLIC FIELDS SHOULD BE CHANGED TO PRIVATE AND SERIALIZED FOR TESTING PURPOSES!!!///
+//Script allows player to move closer and closer towards corner. Work on check to fix this!
+
 public class FollowCam : MonoBehaviour {
-	public GameObject player;
 
-	private Vector2 velocity;
-	public float smoothTimeY;
-	public float smoothTimeX;
+	[SerializeField] Transform character;
 
-	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+	private Vector3 moveTemp;
 
-		transform.position = new Vector3 (posX, posY, transform.position.z);
+	[SerializeField] float speed = 3;
+	[SerializeField] float xDifference;
+	[SerializeField] float yDifference;
+
+	[SerializeField] float movementThreshold = 3;
+
+	void Update () {
+
+		if (character.transform.position.x > transform.position.x) {
+			xDifference = character.transform.position.x - transform.position.x;
+		} else {
+			xDifference = transform.position.x - character.transform.position.x;
+		}
+
+		if (character.transform.position.y > transform.position.y) {
+			yDifference = character.transform.position.y - transform.position.y;
+		} else {
+			yDifference = transform.position.y - character.transform.position.y;
+		}
+
+		if (xDifference >= movementThreshold || yDifference >= movementThreshold) {
+			moveTemp = character.transform.position;
+			moveTemp.z = -1;
+			transform.position = Vector3.MoveTowards (transform.position, moveTemp, speed * Time.deltaTime);
+		}
+			
+			
 	}
 }
