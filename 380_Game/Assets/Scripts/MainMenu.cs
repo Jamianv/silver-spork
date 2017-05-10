2fac7dbf5081f58bb7ae4280dcbd47076a53487f
makdaddy27 <Makdaddy@MacBook-Pro.local>
2017-05-10 16:59:58 -0700

    Added main menu with buttons and what not

---
+++ 380_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public string selectLevel;
+	public string selectOptions;
+
+	public void NewGame()
+	{
+		Application.LoadLevel (selectLevel);
+
+	}
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+	public void Options()
+	{
+		Application.LoadLevel (selectOptions);
+	}
+}
