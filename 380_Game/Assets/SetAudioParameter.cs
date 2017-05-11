3f009dc7bc13c6f04b78c96bb7c547407b6612dd
henryq95 <quinteros7766@gmail.com>
2017-05-10 20:17:48 -0700

    volume sliders for master,music,sfx in options

---
+++ 380_Game/Assets/SetAudioParameter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetAudioParameter : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+
+    public void SetMasterLevel()
+    {
+        float level = masterSlider.value;
+        mixer.SetFloat("masterVol", level);
+    }
+
+    public void SetMusicLevel()
+    {
+        float level = musicSlider.value;
+        mixer.SetFloat("musicVol", level);
+    }
+
+    public void SetSFXLevel()
+    {
+        float level = sfxSlider.value;
+        mixer.SetFloat("sfxVol", level);
+    }
+
+}
